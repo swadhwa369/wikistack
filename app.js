f0408218fec88e5ed8ce8d70cf18fad65c3157c4
John Nevin <jnevin17@gmail.com>
2020-03-12 15:18:28 -0500

    first changes

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const morgan = require("morgan");
+const views = require("./views");
+const app = express();
+const models = require("./models");
+const { db } = require("./models");
+console.log(models);
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/stylesheets"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+  res.send(views.main());
+});
+const PORT = 3000;
+
+const init = async () => {
+  try {
+    await models.db.sync();
+    models.db.sync({ force: true });
+    app.listen(PORT, () => {
+      console.log(`App listening in port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+init();
